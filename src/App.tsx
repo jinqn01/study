09480f73015f88e6330e0af4fd39b596d53face4
jinqn01 <jinqn01@>
2025-03-01 00:01:10 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <p>
+          勉強用        </p>
+        <a
+          className="App-link"
+          href="https://www.youtube.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          YouTube
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
